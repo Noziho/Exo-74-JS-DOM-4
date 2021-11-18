4762e72cb5997256caf66a8bb1b57076879caef4
Noah2211 <noah.decroix3@gmail.com>
2021-11-18 10:08:36 +0100

    exercice 1 done

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,6 +1,18 @@
 //Votre code ci dessous
 
+let imgCat = document.getElementById('cat-pic');
 
+function changeTextCat (){
+    let changeText = document.getElementById('cat-chat');
+    changeText.innerText = "Miaou";
+    return changeText;
+}
+
+imgCat.addEventListener("click", function (){
+
+    changeTextCat ();
+
+});
 
 
 
