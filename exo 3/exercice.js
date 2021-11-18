7d8ace997d1f9be74b3c19d3d46f630f6f770bff
Noah2211 <noah.decroix3@gmail.com>
2021-11-18 10:31:25 +0100

    exercice 3 done

--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,9 +1,16 @@
 let libButton = document.getElementById('lib-button');
 let libIt = function() {
+    let inputNoun = document.getElementById('noun');
+    let inputAdjective = document.getElementById('adjective');
+    let someoneName = document.getElementById('person');
     let storyDiv = document.getElementById("story");
-    storyDiv.innerHTML = "??Your story here";
+    storyDiv.innerHTML = inputNoun.value + " " + inputAdjective.value +" "+ someoneName.value;
+
 };
+
 libButton.addEventListener('click', libIt);
+
+
 
 
 
